c76e0529001db13289624aa054411942cb2ffb4f
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2022-02-02 14:24:33 -0500

    BoardRendererTest only has differences in line separators from failing and passing
    I'm looking for them but its whitespace lol

--- src/main/java/org/example/tictactoe/CommandLineRenderer.java
+++ src/main/java/org/example/tictactoe/CommandLineRenderer.java
@@ -19,7 +19,13 @@ public class CommandLineRenderer {
         *        Check out how the BoardRendererTest JUnit test expects it to be rendered. This method should not call
         *        renderMessage or renderError. Just directly print to this.output
         * */
-        String rowVal = "";
+        /**
+         * 012 3 456 7 8910 11
+         * --- | --- | ---
+         * --- | --- | ---
+         * --- | --- | ---
+         */
+        StringBuilder rowVal = new StringBuilder();
         String cellVal = "";
         String divVal = "|";
         TicTacToeCell[][] renderBoard = board.getCells();
@@ -28,30 +34,32 @@ public class CommandLineRenderer {
         this.output.println(BOARD_BORDER);
         for( int row = 0; row < 3; row++ ){
             for(int col = 0; col<12; col++){
-                if(col%2==0 || col==11) rowVal+=" ";        //stays " " = 0,2,4,6,8,10,11
-                else if(col==3 || col==7) rowVal+=divVal;   //divVal | = 3,7
-                else {                                      //changes val = 1,5,9
-                    trueCol = col%3;
-                    switch ( renderBoard[row][trueCol] ){           // 1mod3 = 1
-                        case EMPTY -> {                     // 5mod3 = 2
-                            rowVal+=" ";                    // 9mod3 = 0
+                if(col%2==0) rowVal.append(" ");        //stays " " = 0,2,4,6,8,10,11
+                else if(col == 11);
+                else if(col==3 || col==7) rowVal.append(divVal);   //divVal | = 3,7
+                else {                                             //changes val = 1,5,9
+                    if(col == 1) trueCol= 0;    // 1mod3 = 1
+                    if(col == 5) trueCol= 1;    // 5mod3 = 2
+                    if(col == 9) trueCol= 2;    // 9mod3 = 0
+                    switch ( renderBoard[row][trueCol] ){
+                        case EMPTY -> {
+                            rowVal.append(" ");
                             break;
                         }
                         case X -> {
-                            rowVal+="X";
+                            rowVal.append("X");
                             break;
                         }
                         case O -> {
-                            rowVal+="O";
+                            rowVal.append("O");
                             break;
                         }
                     }
                 }
             }
 
-            this.output.println(rowVal);
-            rowVal = "";
-            cellVal = "";
+            this.output.print(rowVal+"\n");
+            rowVal = new StringBuilder();
         }
         this.output.println(BOARD_BORDER);
     }
